64ab4b36c35ddac6f9629b85b7aa4512fafaf07a
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-08 00:54:52 +0100

    commit

--- Man_hunter.py
+++ Man_hunter.py
@@ -1,4 +1,4 @@
-print("\nVampire Dracula is finally free.\n Amarjeet has never lost a battle against the undead,but now their fortress is compromised and Dracula is roaming Creeksville.\n It is up to Amarjeet to take the unbeatable monster, but will he be able to contain him? \n")
+print("Vampire Dracula is finally free.\n Amarjeet has never lost a battle against the undead,but now their fortress is compromised and Dracula is roaming Creeksville.\n It is up to Amarjeet to take the unbeatable monster, but will he be able to contain him? \n")
 
 #Loop conditions
 flag = True
