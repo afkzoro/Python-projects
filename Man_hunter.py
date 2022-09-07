b7161da02fdbdcdc15dc5bcd9ff4b5b3049751cd
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-07 17:45:14 +0100

    commit

--- Man_hunter.py
+++ Man_hunter.py
@@ -1,1 +1,4 @@
-print(" Humans have chance to fight back against the vampires.\n They are going to use their weapons and skills to outwit the vampires.\n They have to find a way to be stronger than their enemy.\n The vampires will try their best to capture humans alive.\n The humans will do everything they can do and try not to loose at all costs.\n You decide your next steps using Y for Yes and N for No.\n")
+decide_player = input(" Pick a game character.\n Type in 'vampire' to be a vampire or 'human' to be human:  \n").lower()
+
+if decide_player == "human":
+    print(" Humans have chance to fight back against the vampires.\n They are going to use their weapons and skills to outwit the vampires.\n They have to find a way to be stronger than their enemy.\n The vampires will try their best to capture humans alive.\n The humans will do everything they can do and try not to loose at all costs.\n You decide your next steps using 'Y' for Yes and 'N' for No.\n")
