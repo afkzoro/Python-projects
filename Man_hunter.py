1fc2566c5d16dc854aab519dd80b872dfc4f316e
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-08 03:23:45 +0100

    commit

--- Man_hunter.py
+++ Man_hunter.py
@@ -9,7 +9,29 @@ while flag != False:
     if decide_player == "human":
         flag = False
         print("Amarjeet has a chance to fight back Dracula.\n He is going to use his weapons and skills to outwit Dracula.\n He has to find a way to be stronger than his enemy.\n Dracula will try his best to capture Amarjeet alive.\n Amarjeet will do everything he can do and try not to lose at all costs.\n You decide your next steps using 'Y' for Yes and 'N' for No.\n")
-
+        #human skillsets
+        
+        h_skill_set = ["Gun with silver bullets","Arsonry", "Holywater"]
+        
+        flag = True
+        
+        while flag != False:
+            
+            #pick skill
+            hman_list = int(input(" Pick a skill set:\n 0.Gun with silver bullets\n 1.Arsonry\n 2.Holywater\n Pick a number to continue\n"))
+            
+            if hman_list == 0:
+                flag = False
+                print("You chose Gun with silver bullets.")
+            elif hman_list == 1:
+                flag = False
+                print("You chose Arsonry.")
+            elif hman_list == 2:
+                flag = False
+                print("You chose Holywater.")
+            else:
+                print("Please select the correct input")
+        
     elif decide_player == "vampire":
         flag = False
         print(" Once released,Dracula is a force to be reckone with.\n His powerful hypnotic abilities makes it easy for him to brainwash his victims and he enjoys playing tricks on those who try to stop him.\n Using the power of necromancy and shapeshifting,he's unmatched in his ability to protect himself from any harm.\n He is thirsty for blood,having never drunk from a human since he has been inprisoned four centuries ago.\n You decide your next steps by using 'Y' for yes and 'N' for no \n")
@@ -21,7 +43,7 @@ while flag != False:
         
         while flag != False:
             #Decide
-            vamp_list = int(input(" Pick a skill set:\n 0.Necromancy\n 1.Hypnosis\n 2.Shapeshifting\n"))
+            vamp_list = int(input(" Pick a skill set:\n 0.Necromancy\n 1.Hypnosis\n 2.Shapeshifting\n Pick a number to continue.\n"))
         
             if vamp_list == 0:
                 flag = False
