a101ec45e0cafa2ae9a98744f9aab90f4d5780db
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-09 15:45:48 +0100

    coomit

--- Man_hunter/Man_hunter.py
+++ Man_hunter/Man_hunter.py
@@ -70,6 +70,8 @@ while flag != False:
 		
 		elif dec_3 == 2:
 			print("You chose the right path. Your path is blocked by hypnotized enemies.")
+   
+   
 	elif decide_player == "vampire":
 		flag = False
 		print(" Once released,Dracula is a force to be reckone with.\n His powerful hypnotic abilities makes it easy for him to brainwash his victims and he enjoys playing tricks on those who try to stop him.\n Using the power of necromancy and shapeshifting,he's unmatched in his ability to protect himself from any harm.\n He is thirsty for blood,having never drunk from a human since he has been inprisoned four centuries ago.\n You decide your next steps by using 'Y' for yes and 'N' for no or choosing a number from a list of options. \n")
